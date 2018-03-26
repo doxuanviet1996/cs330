4e5959d2d3fda68f14205df65f90847338f507db
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-03-27 02:30:04 +0900

    .

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -282,7 +282,7 @@ thread_unblock (struct thread *t)
   ASSERT (t->status == THREAD_BLOCKED);
   list_push_back (&ready_list, &t->elem);
   t->status = THREAD_READY;
-  //if(thread_current()->priority < t->priority) thread_yield();
+  if(thread_current()->priority < t->priority) thread_yield();
   intr_set_level (old_level);
 }
 
