953ebd445c476699c012d395ed3c5e401a6ca2c6
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-03-28 09:47:40 +0900

    .

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -280,8 +280,11 @@ thread_unblock (struct thread *t)
   ASSERT (t->status == THREAD_BLOCKED);
   list_push_back (&ready_list, &t->elem);
   t->status = THREAD_READY;
-  int p = thread_current()->priority;
-  if(p < t->priority) thread_yield();
+  if(thread_current()->tid != 1)
+  {
+    int p = thread_current()->priority;
+    if(p < t->priority) thread_yield();
+  }
   intr_set_level (old_level);
 }
 
