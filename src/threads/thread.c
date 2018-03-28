8005bcba3c00a8bd8e04d1b3c5becb569f6d471f
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-03-28 10:43:13 +0900

    .

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -70,6 +70,7 @@ static void kernel_thread (thread_func *, void *aux);
 
 static void idle (void *aux UNUSED);
 static struct thread *running_thread (void);
+static struct thread *best_ready_thread(void);
 static struct thread *next_thread_to_run (void);
 static void init_thread (struct thread *, const char *name, int priority);
 static bool is_thread (struct thread *) UNUSED;
