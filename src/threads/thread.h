de14f8f9d7ea84ff31e64b752f2e1e4d3c6841d9
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-03-28 12:55:11 +0900

    .

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -140,6 +140,6 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
-struct thread *best_thread_among_list (struct list *l);
+static struct thread *best_thread_among_list (struct list *l);
 
 #endif /* threads/thread.h */
