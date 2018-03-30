7a4f50b08e6a028010329e0b5a7051be35588a86
aquacloud <vietdoxuan1996@gmail.com>
2018-03-31 06:27:21 +0900

    .

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -216,7 +216,7 @@ lock_acquire (struct lock *lock)
 
     sema_down (&lock->semaphore);
     lock->holder = thread_current ();
-  }*/
+  }
 }
 
 /* Tries to acquires LOCK and returns true if successful or false
