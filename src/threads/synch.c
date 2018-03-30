826fad83a60e5e230f3ec2e9f882d067e860864e
aquacloud <vietdoxuan1996@gmail.com>
2018-03-31 06:23:00 +0900

    .

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -205,7 +205,7 @@ lock_acquire (struct lock *lock)
   //{
   enum intr_level old_level = intr_disable();
     struct thread *cur = thread_current();
-    printf("NULL lock_acquire called by thread %s\n",cur->name);
+    printf("NULL lock_acquire called by thread\n");
     sema_down (&lock->semaphore);
     printf("Done sema_down\n");
     lock->holder = cur;
