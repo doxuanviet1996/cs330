feda4d51181449610bfd3c6ae7137bcac91016a9
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-04-15 02:45:20 +0900

    .

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -63,7 +63,7 @@ int check_valid(void *ptr)
   void *usr_min_addr = 0x08048000;
   if(!is_user_vaddr(ptr) || ptr < usr_min_addr) exit(-1);
   int *cur_pd = active_pd();
-  if(pagedir_get_page(cur_pd, ptr)) exit(-1);
+  if(!pagedir_get_page(cur_pd, ptr)) exit(-1);
   return 0;
 }
 
