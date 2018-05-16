86e412fded4f6dee24d0cb382fa49e0063ad39b8
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-17 04:50:44 +0900

    .

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -37,7 +37,6 @@ void check_valid(void *ptr, void *esp)
 {
   if(!is_user_vaddr(ptr) || ptr < 0x08048000) exit(-1);
   int *cur_pd = thread_current()->pagedir;
-  // if(!pagedir_get_page(cur_pd, ptr)) exit(-1);
 
   struct sup_page_table_entry *spte = spt_lookup(&thread_current()->spt, ptr);
 
@@ -47,7 +46,7 @@ void check_valid(void *ptr, void *esp)
   // Stack growth - allow to fault 32 bytes below esp.
   if(!spte && esp - ptr <= 32 && stack_grow(ptr)) return;
 
-  exit(-1);
+  if(!pagedir_get_page(cur_pd, ptr)) exit(-1);
 }
 
 void check_valid_str(char *ptr, void *esp)
