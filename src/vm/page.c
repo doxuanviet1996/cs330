7999a611cb10e3e9bbd1624c91cf5477de8de106
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-19 04:30:19 +0900

    .

--- src/vm/page.c
+++ src/vm/page.c
@@ -96,8 +96,7 @@ bool spt_load(struct sup_page_table_entry *spte)
 
 	// SWAP loading
 	if(spte->type == SWAP) return spt_load_swap(spte);
-	if(spte->type == FILE) return spt_load_file(spte);
-	return true;
+	else return spt_load_file(spte);
 }
 
 struct sup_page_table_entry *spt_add_file(void *uaddr, struct file *file, int ofs,
