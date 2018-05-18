57ab8c811006a1bd1e74689978a7198cc5eacd57
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-19 03:38:17 +0900

    .

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -39,11 +39,11 @@ void exit_debug(int err_code)
 
 bool check_valid(void *ptr, void *esp)
 {
-  void *usr_min_addr = 0x08048000;
-  if(!is_user_vaddr(ptr) || ptr < usr_min_addr) exit(-1);
-  int *cur_pd = thread_current()->pagedir;
-  if(!pagedir_get_page(cur_pd, ptr)) exit(-1);
-  return;
+  // void *usr_min_addr = 0x08048000;
+  // if(!is_user_vaddr(ptr) || ptr < usr_min_addr) exit(-1);
+  // int *cur_pd = thread_current()->pagedir;
+  // if(!pagedir_get_page(cur_pd, ptr)) exit(-1);
+  // return;
 
   printf("validate %x\n", ptr);
 
