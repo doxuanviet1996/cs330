6e00d10062231a5201e325588625e3ea85040349
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-19 16:58:16 +0900

    .

--- src/vm/page.c
+++ src/vm/page.c
@@ -22,7 +22,9 @@ bool less_func (const struct hash_elem *a, const struct hash_elem *b, void *aux)
 void destroy_func (struct hash_elem *e, void *aux)
 {
 	struct sup_page_table_entry *spte = hash_entry(e, struct sup_page_table_entry, elem);
-	if(!spte->is_loaded && spte->type == SWAP) spt_load(spte);
+
+	printf("Destroying spte: %p %d", spte->uaddr, spte->type);
+	// if(!spte->is_loaded && spte->type == SWAP) spt_load(spte);
 
 	if(spte->is_loaded)
 	{
