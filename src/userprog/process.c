918ee0984a29106b126337cc21d248a540e2d955
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-19 18:53:32 +0900

    .

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -456,7 +456,7 @@ static bool
 setup_stack (void **esp, char *args, char *save_ptr) 
 {
   if(!stack_grow(PHYS_BASE - PGSIZE)) return false;
-  printf("Thread %s with pagedir %p", thread_current()->name, thread_current()->pagedir);
+  printf("Thread %d, %s with pagedir %p\n", thread_current()->tid, thread_current()->name, thread_current()->pagedir);
   // void *frame = pagedir_get_page(&thread_current()->pagedir, PHYS_BASE - PGSIZE);
   *esp = PHYS_BASE;
   /* Pushing args into stack */
