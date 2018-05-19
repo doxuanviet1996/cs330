d8d8db2aaf862508c186db987c46fd4ee93a1588
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-19 23:18:27 +0900

    .

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -36,10 +36,18 @@ syscall_init (void)
 void check_valid(void *ptr)
 {
   void *usr_min_addr = 0x08048000;
-  if(!is_user_vaddr(ptr) || ptr < usr_min_addr) exit(-1);
+  if(!is_user_vaddr(ptr) || ptr < usr_min_addr)
+  {
+    printf("Error detected in syscall\n");
+    exit(-1);
+  }
   int *cur_pd = thread_current()->pagedir;
-  if(!pagedir_get_page(cur_pd, ptr)) exit(-1);
-  return 0;
+  if(!pagedir_get_page(cur_pd, ptr))
+  {
+    printf("Error detected in syscall\n");
+    exit(-1);
+  }
+  return;
 }
 
 void check_valid_str(char *ptr)
