36846fceac5824f2afc6b92de76f5cd8b4bdb703
doxuanviet1996 <vietdoxuan1996@gmail.com>
2018-05-20 03:53:27 +0900

    .

--- src/vm/page.c
+++ src/vm/page.c
@@ -24,11 +24,11 @@ void destroy_func (struct hash_elem *e, void *aux)
 {
 	struct sup_page_table_entry *spte = hash_entry(e, struct sup_page_table_entry, elem);
 
-	if(!spte->is_loaded && spte->type == SWAP)
-	{
-		if(bitmap_test(swap_used_map, spte->swap_index))
-			bitmap_flip(swap_used_map, spte->swap_index);
-	}
+	// if(!spte->is_loaded && spte->type == SWAP)
+	// {
+	// 	if(bitmap_test(swap_used_map, spte->swap_index))
+	// 		bitmap_flip(swap_used_map, spte->swap_index);
+	// }
 
 	if(spte->is_loaded)
 	{
